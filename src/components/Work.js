5d3e3276a771ba9c22f025f85cc5756bcc6c579e
chetung2808 <chetung2808@gmail.com>
2020-01-19 21:07:59 +0700

    up lai file work va team

--- src/components/Work.js
+++ src/components/Work.js
@@ -95,46 +95,40 @@ export default class Work extends Component {
         <div className="testi-content testi-2-bg">
           <div className="owl-carousel owl-theme owl-loaded">
             <div className="owl-stage-outer"><div className="owl-stage" style={{transform: 'translate3d(-1460px, 0px, 0px)', transition: 'all 0s ease 0s', width: 4380}}><div className="owl-item cloned" style={{width: 730}}><div className="testi-item text-center">
-                    <p>"Breakfast agreeable incommode departure it an. By ignorant at on wondered relation.
-                      Enough at tastes really so cousin am of."</p>
+                    <p>"Đừng bao giờ so sánh bạn với người khác. Nếu bạn làm vậy, bạn đang tự sỉ nhục mình"</p>
                     <div className="author">
-                      <img src="https://techtalk.vn/mark-zuckerberg-tu-bo-nhung-thach-thuc-ca-nhan-hang-nam-cua-minh.html" alt={1} />
-                      <p className="author-text">Nattasha, Aloka CEO</p>
+                      <img src="http://hrchannels.com/uptalent/attachments/images/20190215/1550229057652-Bill-Gates-CEO.jpg" alt={1} />
+                      <p className="author-text">Bill Gates – CEO của Microsoft</p>
                     </div>
-                  </div></div><div className="owl-item cloned" style={{width: 730}}><div className="testi-item text-center">
-                    <p>"There is nothing to prevent us from living on the Moon. Many books have been
-                      written on this subject. ."</p>
+                  </div></div><div className="owl-item cloned" style={{width: 840}}><div className="testi-item text-center">
+                    <p>"Trong một thế giới đang thay đổi rất nhanh chóng, lộ trình duy nhất đưa bạn đến thất bại là không dám mạo hiểm."</p>
                     <div className="author">
-                      <img src="https://drive.google.com/open?id=1T9nGaGb5id8hXSFIlX4bAGvPEoXqjGDy" alt={1} />
-                      <p className="author-text">Rafian Sevent</p>
+                      <img src="http://hrchannels.com/uptalent/attachments/images/20190215/1550229068761-Mark-Zuckerberg-CEO.jpg" alt={1} />
+                      <p className="author-text">Mark Zuckerberg – CEO của Facebook</p>
                     </div>
-                  </div></div><div className="owl-item active" style={{width: 730}}><div className="testi-item text-center">
-                    <p>"Breakfast agreeable incommode departure it an. By ignorant at on wondered relation.
-                      Enough at tastes really so cousin am of."</p>
+                  </div></div><div className="owl-item active" style={{width: 840}}><div className="testi-item text-center">
+                      <p>"Nếu bạn không bỏ cuộc, bạn vẫn còn cơ hội. Bỏ cuộc là thất bại lớn nhất."</p>
                     <div className="author">
-                      <img src="https://drive.google.com/open?id=1XMnyfgRrYP_dViJ9db1_2mNSQjlAXTQ3" alt={1} />
-                      <p className="author-text">Nattasha, Aloka CEO</p>
+                      <img src="http://hrchannels.com/uptalent/attachments/images/20190215/1550229061511-Jack-Ma.jpg" alt={1} />
+                      <p className="author-text">Jack Ma – CEO của Alibaba</p>
                     </div>
-                  </div></div><div className="owl-item" style={{width: 730}}><div className="testi-item text-center">
-                    <p>"There is nothing to prevent us from living on the Moon. Many books have been
-                      written on this subject. ."</p>
+                  </div></div><div className="owl-item" style={{width: 840}}><div className="testi-item text-center">
+                    <p>"Trong cuộc sống, đừng phản ứng, hãy luôn luôn phản hồi."</p>
                     <div className="author">
-                      <img src="https://drive.google.com/open?id=1r__gcFORs4r_oK1M-aUMxvvrGCKV3L53" alt={1} />
-                      <p className="author-text">Rafian Sevent</p>
+                      <img src="http://hrchannels.com/uptalent/attachments/images/20190215/1550229077245-Sundar-Pichai.jpg" alt={1} />
+                      <p className="author-text">Sundar Pichai – CEO của Google</p>
                     </div>
-                  </div></div><div className="owl-item cloned" style={{width: 730}}><div className="testi-item text-center">
-                    <p>"Breakfast agreeable incommode departure it an. By ignorant at on wondered relation.
-                      Enough at tastes really so cousin am of."</p>
+                  </div></div><div className="owl-item cloned" style={{width: 840}}><div className="testi-item text-center">
+                    <p>"Nếu bạn nhân đôi số thí nghiệm bạn làm mỗi năm, bạn sẽ tăng gấp đôi khả năng sáng tạo của mình."</p>
                     <div className="author">
-                      <img src="https://webdevstudios.org/wp-content/uploads/2019/01/Asset-2.png" alt={1} />
-                      <p className="author-text">Nattasha, Aloka CEO</p>
+                      <img src="http://hrchannels.com/uptalent/attachments/images/20190215/1550229064136-Jeff-Beros.jpg" alt={1} />
+                      <p className="author-text">Jeff Beros – CEO của Amazon</p>
                     </div>
-                  </div></div><div className="owl-item cloned" style={{width: 730}}><div className="testi-item text-center">
-                    <p>"There is nothing to prevent us from living on the Moon. Many books have been
-                      written on this subject. ."</p>
+                  </div></div><div className="owl-item cloned" style={{width: 840}}><div className="testi-item text-center">
+                    <p>"Nếu bạn thức dậy vào buổi sáng và nghĩ rằng tương lai sẽ tốt đẹp hơn, đó sẽ là một ngày tươi sáng. Còn ngược lại, đó sẽ không."</p>
                     <div className="author">
-                      <img src="https://webdevstudios.org/wp-content/uploads/2019/01/Asset-2.png" alt={1} />
-                      <p className="author-text">Rafian Sevent</p>
+                      <img src="http://hrchannels.com/uptalent/attachments/images/20190215/1550229059167-Elon-Musk.jpg" alt={1} />
+                      <p className="author-text">Elon Musk – CEO của Tesla, SpaceX và Neuralink</p>
                     </div>
                   </div></div></div></div><div className="owl-nav disabled"><button type="button" role="presentation" className="owl-prev"><span aria-label="Previous">‹</span></button><button type="button" role="presentation" className="owl-next"><span aria-label="Next">›</span></button></div><div className="owl-dots disabled" /></div>
         </div>
@@ -145,37 +139,37 @@ export default class Work extends Component {
     <div className="container">
       <div className="owl-carousel owl-theme owl-loaded owl-drag">
         <div className="owl-stage-outer"><div className="owl-stage" style={{transform: 'translate3d(-1110px, 0px, 0px)', transition: 'all 0s ease 0s', width: 3552}}><div className="owl-item cloned" style={{width: 222}}><div className="parter-item">
-                <img src="https://webdevstudios.org/wp-content/uploads/2019/01/Asset-2.png" alt={1} />
+                <img src="http://esensifiksi.com/tf/creativora/assets/img/partner/3.png" alt={1} />
               </div></div><div className="owl-item cloned" style={{width: 222}}><div className="parter-item">
-                <img src="https://webdevstudios.org/wp-content/uploads/2019/01/Asset-2.png" alt={1} />
+                <img src="http://esensifiksi.com/tf/creativora/assets/img/partner/3.png" alt={1} />
               </div></div><div className="owl-item cloned" style={{width: 222}}><div className="parter-item">
-                <img src="ahttps://webdevstudios.org/wp-content/uploads/2019/01/Asset-2.png" alt={1} />
+                <img src="http://esensifiksi.com/tf/creativora/assets/img/partner/3.png" alt={1} />
               </div></div><div className="owl-item cloned" style={{width: 222}}><div className="parter-item">
-                <img src="https://webdevstudios.org/wp-content/uploads/2019/01/Asset-2.png" alt={1} />
+                <img src="http://esensifiksi.com/tf/creativora/assets/img/partner/3.png" alt={1} />
               </div></div><div className="owl-item cloned" style={{width: 222}}><div className="parter-item">
-                <img src="https://webdevstudios.org/wp-content/uploads/2019/01/Asset-2.png" alt={1} />
+                <img src="http://esensifiksi.com/tf/creativora/assets/img/partner/5.png" alt={1} />
               </div></div><div className="owl-item active" style={{width: 222}}><div className="parter-item">
-                <img src="https://webdevstudios.org/wp-content/uploads/2019/01/Asset-2.png" alt={1} />
+                <img src="http://esensifiksi.com/tf/creativora/assets/img/partner/1.png" alt={1} />
               </div></div><div className="owl-item active" style={{width: 222}}><div className="parter-item">
-                <img src="https://webdevstudios.org/wp-content/uploads/2019/01/Asset-2.png" alt={1} />
+                <img src="http://esensifiksi.com/tf/creativora/assets/img/partner/2.png" alt={1} />
               </div></div><div className="owl-item active" style={{width: 222}}><div className="parter-item">
-                <img src="https://webdevstudios.org/wp-content/uploads/2019/01/Asset-2.png" alt={1} />
+                <img src="http://esensifiksi.com/tf/creativora/assets/img/partner/3.png" alt={1} />
               </div></div><div className="owl-item active" style={{width: 222}}><div className="parter-item">
-                <img src="https://webdevstudios.org/wp-content/uploads/2019/01/Asset-2.png" alt={1} />
+                <img src="http://esensifiksi.com/tf/creativora/assets/img/partner/5.png" alt={1} />
               </div></div><div className="owl-item active" style={{width: 222}}><div className="parter-item">
-                <img src="https://webdevstudios.org/wp-content/uploads/2019/01/Asset-2.png" alt={1} />
+                <img src="http://esensifiksi.com/tf/creativora/assets/img/partner/4.png" alt={1} />
               </div></div><div className="owl-item" style={{width: 222}}><div className="parter-item">
-                <img src="https://webdevstudios.org/wp-content/uploads/2019/01/Asset-2.png" alt={1} />
+                <img src="http://esensifiksi.com/tf/creativora/assets/img/partner/5.png" alt={1} />
               </div></div><div className="owl-item cloned" style={{width: 222}}><div className="parter-item">
-                <img src="https://webdevstudios.org/wp-content/uploads/2019/01/Asset-2.png" alt={1} />
+                <img src="http://esensifiksi.com/tf/creativora/assets/img/partner/1.png" alt={1} />
               </div></div><div className="owl-item cloned" style={{width: 222}}><div className="parter-item">
-                <img src="https://webdevstudios.org/wp-content/uploads/2019/01/Asset-2.png" alt={1} />
+                <img src="http://esensifiksi.com/tf/creativora/assets/img/partner/4.png" alt={1} />
               </div></div><div className="owl-item cloned" style={{width: 222}}><div className="parter-item">
-                <img src="https://webdevstudios.org/wp-content/uploads/2019/01/Asset-2.png" alt={1} />
+                <img src="http://esensifiksi.com/tf/creativora/assets/img/partner/3.png" alt={1} />
               </div></div><div className="owl-item cloned" style={{width: 222}}><div className="parter-item">
-                <img src="https://webdevstudios.org/wp-content/uploads/2019/01/Asset-2.png" alt={1} />
+                <img src="http://esensifiksi.com/tf/creativora/assets/img/partner/2.png" alt={1} />
               </div></div><div className="owl-item cloned" style={{width: 222}}><div className="parter-item">
-                <img src="https://webdevstudios.org/wp-content/uploads/2019/01/Asset-2.png" alt={1} />
+                <img src="http://esensifiksi.com/tf/creativora/assets/img/partner/1.png" alt={1} />
               </div></div></div></div><div className="owl-nav disabled"><button type="button" role="presentation" className="owl-prev"><span aria-label="Previous">‹</span></button><button type="button" role="presentation" className="owl-next"><span aria-label="Next">›</span></button></div><div className="owl-dots disabled" /></div>
     </div>
   </div>
